b7b53fa6384830ba28276f3a6f0013882fa991d4
kukgini <kukgini@gmail.com>
2022-01-23 22:40:55 +0900

    pass empty secret key

--- didcomm-demo-rust/src/main.rs
+++ didcomm-demo-rust/src/main.rs
@@ -1,14 +1,13 @@
 use didcomm_rs::{Message};
 
 fn main() {
-    let sk = vec![];
     let m = Message::new()
         .from("did:xyz:ulapcuhsatnpuhza930hpu34n_")
         .to(&["did:xyz:34r3cu403hnth03r49g03", "did:xyz:03489jnutnjqhiu0uh540u8hunoe"])
         .set_body("This is some payloads".as_bytes());
 
     let ready_to_send = m.as_raw_json().unwrap();
-    let received = Message::receive(&ready_to_send, &sk).unwrap();
+    let received = Message::receive(&ready_to_send, b"123").unwrap();
 
     println!("Hello, I received = {:?}", received);
 }
